3f0fe32cc593f94554eba97e5054eaecf8c2c067
能勢容幸 <system.nosetech@gmail.com>
2022-11-03 19:37:22 +0900

    実体ファイルを再コミット

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -1,1 +1,62 @@
-/Users/hiroyuki/.config/nvim/lua/keymap.lua
+-- ============================================================================
+-- basic operation
+-- ============================================================================
+-- window split
+vim.keymap.set("n", "<C-w>p", "<C-w>v")
+vim.keymap.set("n", "<C-w>P", "<C-w>s")
+
+-- window move
+vim.keymap.set("n", "<C-w>h", "<C-w>h")
+vim.keymap.set("n", "<C-w>j", "<C-w>j")
+vim.keymap.set("n", "<C-w>k", "<C-w>k")
+vim.keymap.set("n", "<C-w>l", "<C-w>l")
+
+-- window resize
+vim.keymap.set("n", "<C-w>H", "5<C-w><")
+vim.keymap.set("n", "<C-w>J", "5<C-w>-")
+vim.keymap.set("n", "<C-w>K", "5<C-w>+")
+vim.keymap.set("n", "<C-w>L", "5<C-w>>")
+
+-- linenumber
+vim.keymap.set("n", "<C-n>", "<cmd>set relativenumber!<cr>")
+
+-- ============================================================================
+-- plugin operation
+-- ============================================================================
+-- Telescope
+vim.keymap.set("n", "<C-p>", "<cmd>Telescope find_files<cr>")
+vim.keymap.set("n", "rg", "<cmd>Telescope live_grep<cr>")
+vim.keymap.set("n", "<C-f>", "<cmd>Telescope frecency<cr>")
+
+-- bufferline
+vim.keymap.set("n", "<Tab>","<cmd>BufferLineCycleNext<cr>")
+vim.keymap.set("n", "<S-Tab>","<cmd>BufferLineCyclePrev<cr>")
+vim.keymap.set("n", "<C-t>l","<cmd>BufferLineMoveNext<cr>")
+vim.keymap.set("n", "<C-t>h","<cmd>BufferLineMovePrev<cr>")
+vim.keymap.set("n", "<C-t>wl","<cmd>BufferLineCloseRight<cr>")
+vim.keymap.set("n", "<C-t>wh","<cmd>BufferLineCloseLeft<cr>")
+vim.keymap.set("n", "<C-t>ww","<cmd>BufferLinePickClose<cr>")
+
+-- Fern
+vim.keymap.set("n", "<C-e>","<cmd>Fern . -reveal=% -drawer -toggle<cr>")
+
+-- gitgutter
+vim.keymap.set("n", "g[","<cmd>GitGutterPrevHunk<cr>")
+vim.keymap.set("n", "g]","<cmd>GitGutterNextHunk<cr>")
+vim.keymap.set("n", "gh","<cmd>GitGutterLineHighlightsToggle<cr>")
+vim.keymap.set("n", "gp","<cmd>GitGutterPreviewHunk<cr>")
+
+-- Coc
+vim.keymap.set("n", "cn","<cmd>CocList diagnostics<cr>")
+vim.keymap.set("n", "cf","<Plug>(coc-codeaction-selected)iw")
+vim.keymap.set("n", "cd","<Plug>(coc-definition)")
+vim.keymap.set("n", "cr","<Plug>(coc-references)")
+vim.keymap.set("n", "c[","<Plug>(coc-diagnostic-prev)")
+vim.keymap.set("n", "c]","<Plug>(coc-diagnostic-next)")
+vim.keymap.set("n", "cp","<cmd>CocCommand markdown-preview-enhanced.openPreview<cr>")
+vim.api.nvim_create_user_command('Format', 'call CocAction(\'format\')', {nargs=0})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+	pattern = { "*.py" },
+	command = 'Format',
+})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,1 +1,52 @@
-/Users/hiroyuki/.config/nvim/lua/plugins.lua
+vim.cmd[[packadd packer.nvim]]
+
+require'packer'.startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- Color Scheme
+  use 'cocopon/iceberg.vim'
+  use 'kaicataldo/material.vim'
+  use 'sainnhe/gruvbox-material'
+
+  -- Statusbar config
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+
+  -- Syntax highlight
+  use 'sheerun/vim-polyglot'
+
+  -- Telescope
+  use{ 'nvim-lua/plenary.nvim'}
+  use{ 'nvim-telescope/telescope.nvim'}
+  use{
+    "nvim-telescope/telescope-frecency.nvim",
+    config = function()
+      require"telescope".load_extension("frecency")
+    end,
+    requires = {"tami5/sqlite.lua"}
+  }
+
+  -- LSP
+  use {'neoclide/coc.nvim', branch = 'release'}
+
+  -- File Explorer
+  use {'lambdalisue/fern.vim'}
+  use {'lambdalisue/fern-git-status.vim'}
+
+  -- git
+  use {'airblade/vim-gitgutter'}
+
+  -- highlighting
+  use 'nvim-treesitter/nvim-treesitter'
+
+  -- Indent guide
+  use 'lukas-reineke/indent-blankline.nvim'
+
+  -- Auto close parentheses
+  use 'cohama/lexima.vim'
+end)
+
